53307d5aa6b67f1bbae30096dcde5158ffc8b081
Eero Turkki <eero.turkki@gmail.com>
2023-09-18 16:04:53 +0300

    all kinds of stuff

--- 1/part1/src/App.jsx
+++ 1/part1/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Hello = (props) => {
+  console.log(props)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
       </p>
-    </>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Leevi', age: 4},
+    { name: 'Venla', age: 10},
+  ]
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
   )
 }
 

90709d2c92f5eedd15c9da6f4ffbe50aa3c2fab2
Eero Turkki <eero.turkki@gmail.com>
2023-09-20 18:29:39 +0300

    harjoitustehtäviä ja unicafen luonti

--- 1/part1/src/App.jsx
+++ 1/part1/src/App.jsx
@@ -1,25 +1,63 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import { useState } from "react"
 
-const App = () => {
-  const friends = [
-    { name: 'Leevi', age: 4},
-    { name: 'Venla', age: 10},
-  ]
-  return (
-    <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-    </div>
+const Display = ({ counter }) => <div>{counter}</div>
+
+
+
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
-}
+
+  const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+  
+  
+    const handleLeftClick = () => {
+      setAll(allClicks.concat('L'))
+      const updatedLeft = left + 1
+      setLeft(updatedLeft)
+      setTotal(updatedLeft + right)
+    }
+  
+  
+    const handleRightClick = () => {
+      setAll(allClicks.concat('R'))
+      const updatedRight = right + 1
+      setRight(updatedRight)
+      setTotal(left + updatedRight)
+    }
+  
+    return (
+      <div>
+        <div>
+          {left}
+          <Button handleClick={handleLeftClick} text='left' />
+          <Button handleClick={handleRightClick} text='right' />
+          {right}
+          <History allClicks={allClicks} />
+        </div>
+      </div>
+    )
+  }
 
 export default App
